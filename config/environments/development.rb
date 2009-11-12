a0a9dca68c48d523daa109ea373554761fd5373a
Robb Broome <robb@teamvoice.com>
2009-11-12 16:07:01 -0600

    cucumber env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,8 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# config.gem "rspec", :lib => false, :version => ">=1.2.2"
+# config.gem "rspec-rails", :lib => false, :version => ">=1.2.2"
+# config.gem "webrat", :lib => false, :version => ">=0.4.3"
+# config.gem "cucumber", :lib => false, :version => ">=0.2.2"
